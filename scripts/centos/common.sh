c4713f350de80902e0922900c5073e01bafe0a9d
Andrew Klaus <andrewklaus@gmail.com>
2017-09-27 13:09:59 -0600

    Adding epel for centos

--- scripts/centos/common.sh
+++ scripts/centos/common.sh
@@ -1,5 +1,6 @@
 sudo yum -y update
 sudo yum -y upgrade
-sudo yum -y install git unzip gcc gcc-c++ make openssl-devel
+sudo yum -y install epel-release git unzip gcc gcc-c++ make openssl-devel
+sudo yum -y update
 
 sudo rm -rf /{root,home/centos}/{.ssh,.bash_history} && history -c
