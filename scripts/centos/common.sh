36a1d91f281d559b6a89b39925fb6dee41998f89
Andrew Klaus <andrewklaus@gmail.com>
2017-09-27 16:19:09 -0600

    New php dependencies

--- scripts/centos/common.sh
+++ scripts/centos/common.sh
@@ -1,7 +1,9 @@
 sudo yum -y update
 sudo yum -y upgrade
-sudo yum -y install epel-release git unzip gcc gcc-c++ make openssl-devel
+sudo yum -y install wget epel-release git unzip gcc gcc-c++ make openssl-devel memcached
 sudo rpm -i http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 sudo yum -y update
 
+sudo systemctl start memcached
+sudo systemctl enable memcached
 sudo rm -rf /{root,home/centos}/{.ssh,.bash_history} && history -c
--- scripts/centos/php.sh
+++ scripts/centos/php.sh
@@ -1,8 +1,22 @@
 sudo yum -y install \
-    php-phpunit-PHPUnit \
-    composer.noarch \
     php56-php \
+    php56-php-pdo \
     php56-php-gmp \
     php56-php-mbstring \
     php56-php-gd \
-    php56-php-cli
+    php56-php-cli \
+    php56-php-pecl-dom-varimport \
+    php56-php-pecl-apcu \
+    php56-php-pecl-apcu-devel \
+    php56-php-pecl-memcache \
+    php56-php-pecl-memcached
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
+wget https://phar.phpunit.de/phpunit-4.8.phar
+chmod +x phpunit-4.8.phar
+sudo mv phpunit-4.8.phar /usr/local/bin/phpunit
