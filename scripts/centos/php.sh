d259379d8bbf1325ae06d8bd4ab48858a45a665f
Andrew Klaus <andrewklaus@gmail.com>
2017-09-27 16:37:59 -0600

    Setting PHP 5.6 as default PHP

--- scripts/centos/php.sh
+++ scripts/centos/php.sh
@@ -1,15 +1,15 @@
 sudo yum -y install \
-    php56-php \
-    php56-php-pdo \
-    php56-php-gmp \
-    php56-php-mbstring \
-    php56-php-gd \
-    php56-php-cli \
-    php56-php-pecl-dom-varimport \
-    php56-php-pecl-apcu \
-    php56-php-pecl-apcu-devel \
-    php56-php-pecl-memcache \
-    php56-php-pecl-memcached
+    php \
+    php-pdo \
+    php-gmp \
+    php-mbstring \
+    php-gd \
+    php-cli \
+    php-pecl-dom-varimport \
+    php-pecl-apcu \
+    php-pecl-apcu-devel \
+    php-pecl-memcache \
+    php-pecl-memcached
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
