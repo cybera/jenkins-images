3476ac40c937f4449f1c75b8de60acb067c7b5bd
Andrew Klaus <andrewklaus@gmail.com>
2017-09-27 14:50:39 -0600

    Adding remi php repo for centos

--- scripts/centos/common.sh
+++ scripts/centos/common.sh
@@ -1,3 +1,4 @@
+sudo rpm -i http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 sudo yum -y update
 sudo yum -y upgrade
 sudo yum -y install epel-release git unzip gcc gcc-c++ make openssl-devel
