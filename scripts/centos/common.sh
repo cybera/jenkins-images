030347d51d51aeaebc0d265e4d9fd79de6074fbe
Andrew Klaus <andrewklaus@gmail.com>
2017-09-27 14:52:56 -0600

    Changing order of remi php repo install

--- scripts/centos/common.sh
+++ scripts/centos/common.sh
@@ -1,7 +1,7 @@
-sudo rpm -i http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 sudo yum -y update
 sudo yum -y upgrade
 sudo yum -y install epel-release git unzip gcc gcc-c++ make openssl-devel
+sudo rpm -i http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 sudo yum -y update
 
 sudo rm -rf /{root,home/centos}/{.ssh,.bash_history} && history -c
