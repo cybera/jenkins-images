d56d25148c7288f054bd290807f0b9774172fcc2
Andrew Klaus <andrewklaus@gmail.com>
2017-09-28 15:22:03 -0600

    Making mysql only listen on localhost for centos

--- scripts/centos/mysql.sh
+++ scripts/centos/mysql.sh
@@ -7,6 +7,21 @@ gpgcheck=1
 EOF
 
 sudo yum -y install MariaDB-server MariaDB-client
+
+sudo tee <<EOF /etc/my.cnf.d/server.cnf >/dev/null
+[server]
+
+[mysqld]
+bind-address = 127.0.0.1
+
+[embedded]
+
+[mariadb]
+
+[mariadb-10.1]
+
+EOF
+
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
 
