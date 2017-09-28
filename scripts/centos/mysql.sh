3eaa1314f2a0fa41f82167ec728392f97546e6f5
Andrew Klaus <andrewklaus@gmail.com>
2017-09-28 14:57:14 -0600

    Adding nodejs dependencies and proper mariadb 5.6 version

--- scripts/centos/mysql.sh
+++ scripts/centos/mysql.sh
@@ -1,3 +1,12 @@
-sudo yum -y install mariadb-server
+sudo tee <<EOF /etc/yum.repos.d/MariaDB.repo > /dev/null
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1
+EOF
+
+sudo yum -y install MariaDB-server MariaDB-client
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
+
---
+++ scripts/centos/nodejs.sh
@@ -0,0 +1,3 @@
+sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install yarn nodejs
