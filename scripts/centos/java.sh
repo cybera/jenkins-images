c62bce080ee6365ef59b298f23f6592efddf3f5c
Andrew Klaus <andrewklaus@gmail.com>
2017-09-29 10:32:15 -0600

    Using OracleJDK8 instead of OpenJDK

--- scripts/centos/java.sh
+++ scripts/centos/java.sh
@@ -1,1 +1,3 @@
-sudo yum -y install java-1.8.0-openjdk-headless java-1.8.0-openjdk-devel
+wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "https://edelivery.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.rpm"
+
+sudo yum localinstall jdk-8u144-linux-x64.rpm
