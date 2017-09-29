38b3851704131905958abcb12949de4e442b237b
Andrew Klaus <andrewklaus@gmail.com>
2017-09-29 11:43:26 -0600

    Needing non-interactive env var set in mysql.sh as well

--- scripts/openconext-java/mysql.sh
+++ scripts/openconext-java/mysql.sh
@@ -1,3 +1,5 @@
+export DEBIAN_FRONTEND="noninteractive"
+
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mariadb.mirror.colo-serv.net/repo/10.1/ubuntu xenial main'
 
