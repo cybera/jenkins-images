3f8bed235eff327d72f282fc5244dded1518bede
Andrew Klaus <andrewklaus@gmail.com>
2017-10-04 14:33:24 -0600

    Changing key for mariadb on trusty

--- scripts/trusty-openconext-java/mysql.sh
+++ scripts/trusty-openconext-java/mysql.sh
@@ -1,5 +1,5 @@
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mariadb.mirror.colo-serv.net/repo/10.1/ubuntu trusty main'
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mariadb.mirror.anstey.ca/repo/10.1/ubuntu trusty main'
 
 sudo apt-get update
 sudo DEBIAN_FRONTEND="noninteractive" apt-get install -y mariadb-server python-mysqldb
