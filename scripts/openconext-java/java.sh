e2392e6f36e148d5c05fc5a06abe2a8282c826b7
Andrew Klaus <andrewklaus@gmail.com>
2017-10-13 12:44:10 -0600

    Merging openconext build servers once again

--- scripts/openconext-java/java.sh
+++
@@ -1,9 +0,0 @@
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-  sudo debconf-set-selections
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt update
-sudo apt install -y oracle-java8-installer oracle-java8-set-default
-
-
