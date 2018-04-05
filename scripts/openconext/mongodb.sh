4b9908d497ebab14317540cda517686bacac19c5
Andrew Klaus <andrewklaus@gmail.com>
2018-04-05 15:28:06 -0600

    Upgrading to Mongodb 3.6

--- scripts/openconext/mongodb.sh
+++ scripts/openconext/mongodb.sh
@@ -1,14 +1,13 @@
 sudo tee <<EOF /etc/yum.repos.d/mongodb.repo > /dev/null
-[mongodb-org-3.4]
+[mongodb-org-3.6]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.6/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc<Paste>
+gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc
 EOF
 
 sudo yum -y install mongodb-org python2-pip
 
 sudo systemctl enable mongod
 sudo systemctl start mongod
-
