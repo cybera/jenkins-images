75212aa0fb1d50d735888992deae3f1c6831250b
Jeremy Nickurak <jeremy@nickurak.ca>
2019-09-04 10:56:22 -0600

    Revert "Remove docker-compose local install instead of docker-machine"
    This reverts commit 66a62e44261350423054ecccb239eca038f2692a.
    
    It turns out this whole approach was flawed, because docker doesn't do
    a good job of packaging and distributing docker-compose and
    docker-machine via their PPA. Back to curl it is.
    
    (╯°□°)╯︵ ┻━┻

--- scripts/ubuntu/docker.sh
+++ scripts/ubuntu/docker.sh
@@ -11,7 +11,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
-curl -L https://github.com/docker/machine/releases/download/v0.12.1/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
-    chmod +x /tmp/docker-machine &&
-    sudo cp /tmp/docker-machine /usr/local/bin/docker-machine &&
-    rm /tmp/docker-machine
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` >/tmp/docker-compose && 
+    chmod +x /tmp/docker-compose &&
+    sudo cp /tmp/docker-compose /usr/local/bin/docker-compose &&
+    rm /tmp/docker-compose
