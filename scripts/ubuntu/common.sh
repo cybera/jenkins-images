86ab2705eb9bd52d0a20e1f67d457ad4e624754d
Jeremy Nickurak <jeremy@nickurak.ca>
2019-09-17 13:34:27 -0600

    More aggressively avoid interactivity
    Setting DEBIAN_FRONTEND=noninteractive seems to prevent some otherwise
    interactive cases (in particular, restarting services as a result of
    an SSL upgrade).

--- scripts/ubuntu/common.sh
+++ scripts/ubuntu/common.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y git default-jre software-properties-common build-essential
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -q -y
+sudo apt-get install -q -y git default-jre software-properties-common build-essential
 
 sudo rm -rf /{root,home/ubuntu}/{.ssh,.bash_history} && history -c
